a7bbbcea1c17182e67e2ed87dc9be164488ce66f
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-07-01 18:45:32 +0200

    Ran black

--- client/settings/development.py
+++ client/settings/development.py
@@ -4,8 +4,10 @@ This is the configuration file for the TOSTI fridge client.
 from scanners import Console
 
 SCANNER_CLASS = Console
-CLIENT_SECRET = "wYQ8OJyEK85G7bN6DFuE5SekD8noHK0R3CoF6piVtX68Mv21vyQxhcmXtsJtR7y5dDgw7a0ldOLOcH4mYFC8ziyPVgQZjjoS" \
-                "R8azEjcVO01TWpAfT3p3aC0uA9Z1qMUY"
+CLIENT_SECRET = (
+    "wYQ8OJyEK85G7bN6DFuE5SekD8noHK0R3CoF6piVtX68Mv21vyQxhcmXtsJtR7y5dDgw7a0ldOLOcH4mYFC8ziyPVgQZjjoS"
+    "R8azEjcVO01TWpAfT3p3aC0uA9Z1qMUY"
+)
 CLIENT_ID = "SwdUUTX5vdtsh0HIQxpQ4oZJHYWxz6mNfLkwWUR9"
 API_BASE_URL = "http://localhost:8000"
 
