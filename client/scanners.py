5485e6768f2bf15c183d7598cc785a8a1f0257f3
Lars van Rhijn <larsvanrhijn@gmail.com>
2023-07-01 18:52:41 +0200

    Added camera support

--- client/scanners.py
+++ client/scanners.py
@@ -1,3 +1,6 @@
+import cv2
+
+
 class Scanner:
     """Abstract scanner class."""
 
@@ -12,3 +15,22 @@ class Console(Scanner):
     def scan(self):
         """Scan from terminal."""
         return input("Provide new input: ")
+
+
+class Camera(Scanner):
+    """Camera input scanner."""
+
+    def __init__(self, camera_id=0):
+        """Initialize the camera stream."""
+        self.cap = cv2.VideoCapture(camera_id)
+        self.detector = cv2.QRCodeDetector()
+
+    def scan(self):
+        """Scan from a camera image."""
+        _, img = self.cap.read()
+        data, _, _ = self.detector.detectAndDecode(img)
+        return data
+
+    def __del__(self):
+        """Stop camera stream."""
+        self.cap.release()
