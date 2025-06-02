4ea393dbd469fc1fdcb3f35e45b8d6475404761b
Job Doesburg <job.doesburg@gmail.com>
2025-06-02 19:52:58 +0200

    Remove webcam scanner

--- client/scanners.py
+++ client/scanners.py
@@ -1,7 +1,3 @@
-import cv2
-from pyzbar.pyzbar import decode
-
-
 class Scanner:
     """Abstract scanner class."""
 
@@ -17,28 +13,3 @@ class Console(Scanner):
         """Scan from terminal."""
         return input("Provide new input: ")
 
-
-class Camera(Scanner):
-    """Camera input scanner."""
-
-    def __init__(self, camera_id=0):
-        """Initialize the camera stream."""
-        self.cap = cv2.VideoCapture(camera_id)
-        self.cap.set(cv2.CAP_PROP_BRIGHTNESS, 0)
-        self.cap.set(cv2.CAP_PROP_GAIN, 200)
-        self.cap.set(cv2.CAP_PROP_SATURATION, 200)
-        self.detector = cv2.QRCodeDetector()
-
-    def scan(self):
-        """Scan from a camera image."""
-        _, img = self.cap.read()
-        cv2.normalize(img, img, 0, 255, cv2.NORM_MINMAX)
-        data = decode(img)
-        if len(data) > 0:
-            data = data[0]
-            return data.data.decode("utf-8")
-        return None
-
-    def __del__(self):
-        """Stop camera stream."""
-        self.cap.release()
