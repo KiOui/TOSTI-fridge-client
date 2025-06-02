f2485b3235eb0881b9ccda2882e36b05fd5c8642
Job Doesburg <job.doesburg@gmail.com>
2025-06-02 20:12:20 +0200

    Move production.py to not be a template anymore

--- client/settings/production.py
+++ client/settings/production.py
@@ -1,14 +1,16 @@
 """
 This is the configuration file for the TOSTI fridge client.
 """
-from scanners import Camera
+import os
+
 from fridge import Fridge
+from scanners import Console
 
 SCANNER_CLASS = Console
 FRIDGE_CLASS = Fridge
-CLIENT_SECRET = "[Client Secret]"
-CLIENT_ID = "[Client ID]"
-API_BASE_URL = "https://tosti.science.ru.nl"
+CLIENT_SECRET = os.getenv('TOSTI_CLIENT_SECRET')
+CLIENT_ID = os.getenv('TOSTI_CLIENT_ID')
+API_BASE_URL = os.getenv('TOSTI_API_BASE_URL', 'https://tosti.science.ru.nl')
 
 LOCK_CONFIGURATION = [
     {
